71906e4101f7d86b6777fd38c812cb620f7c85ff
Mohan <157688677+Mownick@users.noreply.github.com>
2025-06-26 14:41:49 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-from ui_main import SecureFileStorageApp
-import sys
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = SecureFileStorageApp()
-    window.show()
-    sys.exit(app.exec_())
